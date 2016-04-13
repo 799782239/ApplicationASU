0310ce5a49e71b190bc08ca1d938e162e86fdf9b
799782239 <799782239@qq.com>
2016-04-13 19:03:17 +0800

    修复重新登录计时器崩溃bug

--- app/src/main/java/com/example/yan/myapplication/utils/TimerUtils.java
+++ app/src/main/java/com/example/yan/myapplication/utils/TimerUtils.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.yan.myapplication.model.SaveData;
 
@@ -18,22 +19,27 @@ public class TimerUtils extends TimerTask {
     private Handler mHandler;
 
     public TimerUtils(Handler mHandler) {
-        this.mHandler = mHandler;
+        if (this.mHandler == null) {
+            this.mHandler = mHandler;
+        }
     }
 
     @Override
     public void run() {
-        if (SaveData.data.getAsu() != 0&&SaveData.data!=null) {
-            Message msg = new Message();
-            Bundle data = new Bundle();
-            data.putInt("ASU", SaveData.data.getAsu());
-            data.putDouble("Latitude", SaveData.data.getLaLatitude());
-            data.putDouble("Longitude", SaveData.data.getLongitude());
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-            data.putString("time", sdf.format(System.currentTimeMillis()));
-            msg.setData(data);
-            msg.what = 1;
-            mHandler.sendMessage(msg);
+        if (SaveData.data == null) {
+        } else {
+            if (SaveData.data.getAsu() != 0) {
+                Message msg = new Message();
+                Bundle data = new Bundle();
+                data.putInt("ASU", SaveData.data.getAsu());
+                data.putDouble("Latitude", SaveData.data.getLaLatitude());
+                data.putDouble("Longitude", SaveData.data.getLongitude());
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                data.putString("time", sdf.format(System.currentTimeMillis()));
+                msg.setData(data);
+                msg.what = 1;
+                mHandler.sendMessage(msg);
+            }
         }
 
     }
